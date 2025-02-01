c539592818d0602857cdedbd28bb7ffbd800dc80
Doug <rdcassin@gmail.com>
2025-01-31 21:16:53 -0600

    Set up clerk webhook, prisma, and ngrok.  About to start Nav Bar

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhooks(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
+  
   if (!isPublicRoute(request)) {
     await auth.protect()
   }
